bd2943247baf73c43caa6a8ae5abbb4a50124e12
subvair <vadimogienko42k@gmail.com>
2024-10-12 15:16:15 +0700

    Вывел в функции ввод масивов и проверку на условие

--- main.cpp
+++ main.cpp
@@ -2,7 +2,25 @@
 
 using namespace std;
 
+void fillingArray(int arr[], int size){
+    for (int i = 0; i < size; ++i) {
+        cout << "Введите " << i << "-й элемент массива"<< endl;
+        cin >> arr[i];
+    }  
+}
+
+int checkArray(int arr[], int size, int a, int b){
+    int count = 0;
+    for (int i = 0; i < size; ++i) {
+        if (arr[i]>0 and a <= arr[i] and arr[i] <= b) {
+            ++count;
+        }
+    }
+    return count;
+}
+
 int main() {
+    setlocale(LC_ALL, "RU");
     int a, b, k, n;
 
     cout << "Введите границы интервала через пробел(a b)" << endl;
@@ -10,40 +28,18 @@ int main() {
 
     cout << "Введите размерность массива V" << endl;
     cin >> k;
-
     int v[k];
-
-    for (int i = 0; i < k; ++i) {
-        cout << "Введите " << i << "-й элемент массива" << endl;
-        cin >> v[i];
-    }
+    fillingArray(v, k);
 
     cout << "Введите размерность массива W" << endl;
     cin >> n;
-
     int w[n];
+    fillingArray(w, n);
+
+    int countV, countW;
+    countV = checkArray(v, k, a, b); 
+    countW = checkArray(w, n, a, b);
     
-    for (int i = 0; i < n; ++i) {
-        cout << "Введите " << i << "-й элемент массива" << endl;
-        cin >> w[i];
-    }
-    
-    int countV = 0, countW = 0;
-
-    cout << countV << " " << countW;
-
-    for (int i = 0; i < k; ++i) {
-        if (v[i]>0 and a <= v[i] and v[i] <= b) {
-            ++countV;
-        }
-    } 
-
-    for (int i = 0; i < n; ++i) {
-        if (v[i]>0 and a <= v[i] and v[i] <= b) {
-            ++countW;
-        }
-    }      
-
     cout << "\nКоличество подходящих элементов в массиве V: "<<countV << "\nКоличество подходящих элементов в массиве W: " << countW;
 
 }
